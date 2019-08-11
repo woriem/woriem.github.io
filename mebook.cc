cb0fda723c58fcb9da5e1c1357cd66d0503d7d11
woriem <38534676+woriem@users.noreply.github.com>
2019-08-11 18:17:09 +0800

    Create mebook.cc

---
+++ mebook.cc
@@ -0,0 +1,22 @@
+{
+  "bookSourceGroup": "下载",
+  "bookSourceName": "小小书屋➣",
+  "bookSourceUrl": "http://mebook.cc",
+  "enable": true,
+  "ruleBookContent": "text",
+  "ruleChapterList": "class.list@tag.a",
+  "ruleChapterName": "text",
+  "ruleChapterUrl": "class.downbtn@href",
+  "ruleContentUrl": "href<js>result.replace(\"baidu\",\"baiduwp\")</js>",
+  "ruleFindUrl": "多看专区::http://mebook.cc/category/duokan/page/searchPage\n合集资源::http://mebook.cc/category/hjzy/page/searchPage\n网络小说::http://mebook.cc/tag/网络小说/page/searchPage",
+  "ruleSearchAuthor": "tag.h2.a.0@text#《.*》|\\（.*",
+  "ruleSearchCoverUrl": "img@src",
+  "ruleSearchIntroduce": "tag.p@text#内容简介|阅读全文.*",
+  "ruleSearchKind": "class.cat@tag.a@text",
+  "ruleSearchList": "class.list@tag.li",
+  "ruleSearchName": "tag.h2.a.0@text#》.*|《",
+  "ruleSearchNoteUrl": "class.img@tag.a.0@href",
+  "ruleSearchUrl": "http://mebook.cc/?s=searchKey",
+  "serialNumber": 31,
+  "weight": 0
+}
